f0c13fe8a0b7e8c611a6d1d97816d99563896126
Thomas Schraitle <tom_schr@web.de>
2017-12-03 15:42:39 +0100

    Provide xml2pong
    * Provide scripts key in setup.py
    * Rename xml2pong.py -> xml2pong

--- setup.py
+++ setup.py
@@ -154,9 +154,10 @@ setupdict = dict(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': ['xml2pong=xml2pong.cli:main'],
-    },
+    #entry_points={
+    #    'console_scripts': ['xml2pong=xml2pong.cli:main'],
+    #},
+    scripts = ['bin/xml2pong'],
 
     # Required packages for using "setup.py test"
     setup_requires=['pytest-runner'],
