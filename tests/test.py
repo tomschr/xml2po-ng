7d9ffe9a5d47d7be95105de2b9ba5e666efade78
Thomas Schraitle <tom_schr@web.de>
2017-12-10 13:15:50 +0100

    Adapt test.py for Python3 and global xml2po

--- tests/test.py
+++ tests/test.py
@@ -25,25 +25,25 @@ if len(sys.argv) > 1:
         for opt in sys.argv[2:]:
             myopts += " " + opt
     output = input.replace(".xml", ".xml.out")
-    fullcommand = "../xml2po/xml2po %s %s | sed 's/\"POT-Creation-Date: .*$/\"POT-Creation-Date: \\\\n\"/' | diff -u %s -" % (myopts, input, pot)
+    fullcommand = "xml2po %s %s | sed 's/\"POT-Creation-Date: .*$/\"POT-Creation-Date: \\\\n\"/' | diff -u %s -" % (myopts, input, pot)
     #print >>sys.stderr, fullcommand
     ret = os.system(fullcommand)
     if ret:
-        print "Problem: extraction from '%s'" % (input)
-    fullcommand = "../xml2po/xml2po -p %s %s %s | diff -u %s -" % (po, myopts, input, output)
+        print("Problem: extraction from '%s'" % (input))
+    fullcommand = "xml2po -p %s %s %s | diff -u %s -" % (po, myopts, input, output)
     #print >>sys.stderr, fullcommand
     ret = os.system(fullcommand)
     if ret:
-        print "Problem: merging translation into '%s'" % (input)
+        print("Problem: merging translation into '%s'" % (input))
 else:
     time_start = time.time()
     for t in SIMPLETESTS:
         myopts = SIMPLETESTS[t].get("options", "")
         if os.system("%s %s %s" % (sys.argv[0], t, myopts)):
-            print "WARNING: Test %s failed." % (t)
+            print("WARNING: Test %s failed." % (t))
 
     for t in OTHERTESTS:
         if os.system("cd %s && ./%s" % (t[0], t[1])):
-            print "WARNING: Test %s failed." % (t[0])
+            print("WARNING: Test %s failed." % (t[0]))
     time_end = time.time()
-    print "%d tests executed in %2.2f secs" % (len(SIMPLETESTS)+len(OTHERTESTS), time_end-time_start)
+    print("%d tests executed in %2.2f secs" % (len(SIMPLETESTS)+len(OTHERTESTS), time_end-time_start))
